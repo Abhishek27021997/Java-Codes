c024ff13a8d30146dd7c0500ee2330653e98b778
frost <frostabbi3@gmail.com>
2023-10-12 16:19:31 +0530

    OOPS Concept

--- OOPS_Concepts/src/Polymorphism.java
+++ OOPS_Concepts/src/Polymorphism.java
@@ -29,37 +29,33 @@ import java.util.*;
                       //Function Overriding
 
 //Base Class
-class Parent {
-	void show() 
-	{ 
-		System.out.println("Parent's show()"); }
-}
-
-//Inherited class
-class Child extends Parent {
-	// This method overrides show() of Parent
-	@Override void show()
-	{
-		System.out.println("Child's show()");
-	}
-}
-
-//Driver class
-class Polymorphism {
-	public static void main(String[] args)
-	{
-		// If a Parent type reference refers
-		// to a Parent object, then Parent's
-		// show is called
-		Parent obj1 = new Parent();
-		obj1.show();
-
-		// If a Parent type reference refers
-		// to a Child object Child's show()
-		// is called. This is called RUN TIME
-		// POLYMORPHISM.
-		Parent obj2 = new Child();
-		obj2.show();
-	}
-}  
+//class Parent {
+//	void show()
+//	{
+//		System.out.println("Parent's show()"); }
+//}
+//
+////Inherited class
+//class Child extends Parent {
+//	// This method overrides show() of Parent
+//	@Override void show()
+//	{
+//		System.out.println("Child's show()");
+//	}
+//}
+//
+//class Polymorphism {
+//	public static void main(String[] args)
+//	{
+//
+//		// If a Parent type reference refers
+//		// to a Parent object, then Parent's
+//		// show is called
+//		Parent obj1 = new Parent();
+//		obj2.show();
+//
+//		Parent obj2 = new Child();
+//		obj2.show();
+//	}
+//}
 
